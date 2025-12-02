7860e5ce0dc1e6f21d5ecac44edc3a6cd6965e6c
Kenny <51289140+CcydtN@users.noreply.github.com>
2025-12-01 21:25:06 -0500

    Day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,10 +1,55 @@
 advent_of_code::solution!(1);
+use anyhow::Result;
+use anyhow::anyhow;
+use num::traits::Euclid;
 
-pub fn part_one(input: &str) -> Option<u64> {
-    None
+enum Direction {
+    Left(i64),
+    Right(i64),
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+impl TryFrom<&str> for Direction {
+    type Error = anyhow::Error;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let (dir, val) = value.split_at(1);
+        match dir {
+            "L" => Ok(Direction::Left(val.parse()?)),
+            "R" => Ok(Direction::Right(val.parse()?)),
+            _ => Err(anyhow!("Input Error")),
+        }
+    }
+}
+
+fn parseInput(input: &str) -> Result<Vec<Direction>> {
+    input
+        .lines()
+        .map(|value| Direction::try_from(value))
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<i64> {
+    let inputs = parseInput(input).expect("Parse Error");
+    let mut dial = 50i64;
+    let mut password = 0;
+    for input in inputs {
+        match input {
+            Direction::Left(val) => {
+                dial -= val;
+            }
+            Direction::Right(val) => {
+                dial += val;
+            }
+        }
+        dial = dial.rem_euclid(100);
+        println!("{:?}", dial);
+        if dial == 0 {
+            password += 1;
+        }
+    }
+    Some(password)
+}
+
+pub fn part_two(input: &str) -> Option<i64> {
     None
 }
 
@@ -15,7 +60,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
