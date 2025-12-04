c667e81f82c4c34f9cb9167dd6461f6d78c51a60
Kenny <51289140+CcydtN@users.noreply.github.com>
2025-12-03 19:39:42 -0500

    Day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -53,6 +53,47 @@ impl Deref for Bank {
     }
 }
 
+fn recursive(stack: &mut Vec<usize>, buffer: &[Vec<usize>], size: usize) -> bool {
+    if stack.len() == size {
+        return true;
+    }
+    for i in (1..=9).rev() {
+        let idx;
+        if let Some(x) = stack.last() {
+            idx = buffer[i].iter().find(|&v| v > x)
+        } else {
+            idx = buffer[i].first()
+        };
+        if idx.is_none() {
+            continue;
+        }
+        stack.push(*idx.unwrap());
+        if recursive(stack, buffer, size) {
+            return true;
+        }
+        stack.pop();
+    }
+
+    false
+}
+
+fn compute_joltages(bank: &[Battery], size: usize) -> Option<u64> {
+    let mut stack = vec![];
+    let mut buffer = vec![vec![]; 10];
+    for (i, battery) in bank.iter().enumerate() {
+        buffer[battery.0 as usize].push(i);
+    }
+    let _ = recursive(&mut stack, &buffer, size);
+    Some(
+        stack
+            .into_iter()
+            .rev()
+            .enumerate()
+            .map(|(i, val)| bank[val].0 * 10u64.pow(i as u32))
+            .sum(),
+    )
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let inputs = input
         .lines()
@@ -65,33 +106,30 @@ pub fn part_one(input: &str) -> Option<u64> {
 
     let mut sum = 0u64;
     for bank in inputs {
-        let mut buffer = vec![vec![]; 10];
-        for (i, battery) in bank.iter().enumerate() {
-            buffer[battery.0 as usize].push(i);
-        }
-        // dbg!(&buffer);
-        'outer: for i in (1..=9u64).rev() {
-            let Some(start) = buffer[i as usize].first() else {
-                continue;
-            };
-            for j in (1..=9u64).rev() {
-                let Some(end) = buffer[j as usize].last() else {
-                    continue;
-                };
-                if start >= end {
-                    continue;
-                }
-                sum += i * 10 + j;
-                // dbg!(i * 10 + j);
-                break 'outer;
-            }
-        }
+        let joltages = compute_joltages(&bank, 2)?;
+        // dbg!(&bank, &joltages);
+        sum += joltages;
     }
+
     Some(sum)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let inputs = input
+        .lines()
+        .map(Bank::try_from)
+        .inspect(|val| {
+            // dbg!(val);
+        })
+        .collect::<Result<Vec<_>>>()
+        .expect("Parse input error");
+    let mut sum = 0u64;
+    for bank in inputs {
+        let joltages = compute_joltages(&bank, 12)?;
+        // dbg!(&bank, &joltages);
+        sum += joltages;
+    }
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -107,6 +145,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
